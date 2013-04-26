12fad63171bef0e676c59a07caf4815314ac3c70
Yuval Carmel <yuval9859@gmail.com>
2013-04-26 18:55:10 +0300

    Ignoring Backlog xls Changing comment in GPSTracker

--- src/com/yuvalc/hummusdroid/GPSTracker.java
+++ src/com/yuvalc/hummusdroid/GPSTracker.java
@@ -31,10 +31,10 @@ public class GPSTracker extends Service implements LocationListener {
 	double longitude; // longitude
 
 	// The minimum distance to change Updates in meters
-	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+	private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10m
 
 	// The minimum time between updates in milliseconds
-	private static final long MIN_TIME_BW_UPDATES = 10000; // 1 minute
+	private static final long MIN_TIME_BW_UPDATES = 10000; // 10 seconds
 
 	// Declaring a Location Manager
 	protected LocationManager locationManager;
